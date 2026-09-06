34c9e850151b66c8654cfec3f9e5c905ab5ca44d
Gajanan Deshmukh <159412495+Gajanand1219@users.noreply.github.com>
2026-09-06 13:24:59 +0530

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,36 +4,31 @@ import { useLanguage } from '../context/LanguageContext'
 
 // =====================================================
 // GALLERY GROUPS
-// प्रत्येक g च्या आत त्याची स्वतःची images आहेत.
+// प्रत्येक group च्या आत त्याची स्वतःची images आहेत.
 // =====================================================
 
 const galleryGroups = [
   // ---------------------------------------------------
   // G1 - LOGO
   // ---------------------------------------------------
- 
-
-
-
-
-{
- id: 'g1',
+  {
+    id: 'g1',
     cover: logo,
     labelMr: 'आमचा ब्रँड',
     labelEn: 'Our Brand',
 
     images: [
-       {
+      {
         src: logo,
         alt: 'Shivyoga Electrical & Electronics Logo',
       },
-    {
-        src: 'https://scontent.fhyd7-1.fna.fbcdn.net/v/t39.30808-6/646806057_122102622602365627_7086416858809416304_n.jpg?stp=dst-jpg_tt6&cstp=mx2048x2048&ctp=s960x960&_nc_cat=105&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=5kjpZAGTxf4Q7kNvwGKIBJ_&_nc_oc=Adq56qyfjcNn2lZlxLM_AKIgMPk2VPYGSIbAfV_YQPB_ScDJV5BdzQrJpIDXUQ5HQyI&_nc_zt=23&_nc_ht=scontent.fhyd7-1.fna&_nc_gid=Nwo5M2VdlD7-_kdVbmR8NQ&_nc_ss=7b2a8&oh=00_AQK51A2r19WDSwDbIGQYK7PqwVsMT01eS2yOIcgKwufa_Q&oe=6A9A3B84',
+      {
+        src: 'https://scontent.fhyd7-1.fna.fbcdn.net/v/t39.30808-6/646806057_122102622602365627_7086416858809416304_n.jpg?stp=dst-jpg_tt6&cstp=mx2048x2048&ctp=s2048x2048&_nc_cat=105&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=qRUPOc6POuoQ7kNvwHc34Cx&_nc_oc=Adq_qq1NlcOT6n_kF68lTGnz0qDLQ5ARYyU753HBkjh2V34hWj5JsOETc4df6Urc2Es&_nc_zt=23&_nc_ht=scontent.fhyd7-1.fna&_nc_gid=Zwr5ZzmbveukdtjJ2zzN9Q&_nc_ss=7b2a8&oh=00_AQI3j9DH4bfGQts6F6QBT6trlWojAFhBo7-M19Km2eGbeQ&oe=6AA30584',
         alt: 'Shivyoga Electrical & Electronics Logo',
-      }
-      
+      },
     ],
   },
+
   // ---------------------------------------------------
   // G2 - SHOP
   // ---------------------------------------------------
@@ -44,7 +39,7 @@ const galleryGroups = [
     labelEn: 'Shop',
 
     images: [
-        {
+      {
         src: 'https://cdn.corenexis.com/f/1Snft4Sr9KF.jpeg',
         alt: 'Shivyoga Shop',
       },
@@ -52,7 +47,7 @@ const galleryGroups = [
         src: 'https://cdn.corenexis.com/f/ICCUOT7xdEm.png',
         alt: 'Shivyoga Shop',
       },
-       {
+      {
         src: 'https://lh3.googleusercontent.com/gps-cs-s/AHRPTWl4KetbUklNYG9PP1iCtxbXAtw9dIA8RPpULLTydKeQ98PXo81jlYoht0NPvJ1ngXl1J5pwKGkT2hlaqHmU4OY8Sxix3rdP3tU3HpKFOctSOH2ZMt0l19IHk0Ge2JM1bySo10JIgW2qBGwi=w203-h270-k-no',
         alt: 'Shivyoga Electrical Shop',
       },
@@ -60,7 +55,6 @@ const galleryGroups = [
         src: 'https://cdn.corenexis.com/f/EVuoVxe44fS.png',
         alt: 'Shivyoga Shop Interior',
       },
-    
       {
         src: 'https://cdn.corenexis.com/f/01HoYBZZe8j.png',
         alt: 'Shivyoga Electrical Shop',
@@ -69,50 +63,45 @@ const galleryGroups = [
         src: 'https://lh3.googleusercontent.com/gps-cs-s/AHRPTWkImTjsF1AFT15fU6Nzum6Gn12-jalgnE3eWRyiDkKk5bsur8d5uDl2VG_mFZd3627ciVRYKYbb_nszpwugXUGCcJdI5bmHRjVZAYKHtNGnIIw4uFg8qqUH-zjrMt6kdVQ6Ygsv2L4mbgH6=s527-k-no',
         alt: 'Shivyoga Shop Products',
       },
-       {
+      {
         src: 'https://lh3.googleusercontent.com/gps-cs-s/AHRPTWn81dFXDG1tsHX5o8ZHg3M8cDCHovBfVIm7Q7CHSwLhCjZbI83lDhGLcQTKSt88k7Rg6ClfrPO5ITZR3A2kgN7-l-7xUTouaVqmfKpsj9Ug7ft-SUfsZkDxUfzAW7Gvduk4RyEtnP2j43Z7=w203-h270-k-no',
         alt: 'Shivyoga Shop Products',
       },
-
-      
     ],
   },
 
-  
   // ---------------------------------------------------
   // G9 - FESTIVAL DECORATION
   // ---------------------------------------------------
   {
     id: 'g9',
-    cover: 'https://scontent.fhyd7-1.fna.fbcdn.net/v/t39.30808-6/779957641_122130962228365627_2655053558510186934_n.jpg?stp=dst-jpg_tt6&cstp=mx1024x1536&ctp=s1024x1536&_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=y1IwIj347NYQ7kNvwFon37_&_nc_oc=Adp4V0Kww9m7OR4sy07CQJp3HOgA59R81XgD1KWs-sB1-XigOlYhv7i-yx1HAwjRd4A&_nc_zt=23&_nc_ht=scontent.fhyd7-1.fna&_nc_gid=nUBCDhf6n_n-pZxavqnYrQ&_nc_ss=7b2a8&oh=00_AQL1z3WYvAbUfjw43Ann6n4L7h_QVPXxkKRiZosw9elkhw&oe=6A9A3FAE',
+    cover:
+      'https://scontent.fhyd7-1.fna.fbcdn.net/v/t39.30808-6/797193882_122131519706365627_5088582712310782946_n.jpg?stp=dst-jpg_tt6&cstp=mx1536x1024&ctp=s1536x1024&_nc_cat=108&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=833d8c&_nc_ohc=SG0U1mzQtKkQ7kNvwE-W6gU&_nc_oc=Adr72il4J5l7bkT5rK48y_VKK_p8JJSDnep-45LGfQ_Jq3KwVeT2hSZ5VSkbIuZ-9AE&_nc_zt=23&_nc_ht=scontent.fhyd7-1.fna&_nc_gid=u5KAJ3xA6S0Ve3yUtZWKpQ&_nc_ss=7b2a8&oh=00_AQL2saqiWqFeOnlQkqBAerPtkXHlx0E5dR8YDydW5nJLww&oe=6AA2D423',
     labelMr: 'सण सजावट',
     labelEn: 'Festival Decoration',
 
     images: [
       {
-        src: 'https://scontent.fhyd7-1.fna.fbcdn.net/v/t39.30808-6/779957641_122130962228365627_2655053558510186934_n.jpg?stp=dst-jpg_tt6&cstp=mx1024x1536&ctp=s1024x1536&_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=y1IwIj347NYQ7kNvwFon37_&_nc_oc=Adp4V0Kww9m7OR4sy07CQJp3HOgA59R81XgD1KWs-sB1-XigOlYhv7i-yx1HAwjRd4A&_nc_zt=23&_nc_ht=scontent.fhyd7-1.fna&_nc_gid=nUBCDhf6n_n-pZxavqnYrQ&_nc_ss=7b2a8&oh=00_AQL1z3WYvAbUfjw43Ann6n4L7h_QVPXxkKRiZosw9elkhw&oe=6A9A3FAE',
+        src: 'https://scontent.fhyd7-1.fna.fbcdn.net/v/t39.30808-6/797193882_122131519706365627_5088582712310782946_n.jpg?stp=dst-jpg_tt6&cstp=mx1536x1024&ctp=s1536x1024&_nc_cat=108&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=833d8c&_nc_ohc=SG0U1mzQtKkQ7kNvwE-W6gU&_nc_oc=Adr72il4J5l7bkT5rK48y_VKK_p8JJSDnep-45LGfQ_Jq3KwVeT2hSZ5VSkbIuZ-9AE&_nc_zt=23&_nc_ht=scontent.fhyd7-1.fna&_nc_gid=u5KAJ3xA6S0Ve3yUtZWKpQ&_nc_ss=7b2a8&oh=00_AQL2saqiWqFeOnlQkqBAerPtkXHlx0E5dR8YDydW5nJLww&oe=6AA2D423',
         alt: 'Festival Decoration',
       },
-       {
+      {
         src: 'https://alacritys.in/wp-content/uploads/2024/09/1.-Fairy-Light-Curtains-Ganpati-Decor.jpeg',
         alt: 'Festival Decoration',
       },
       {
-        src: 'https://scontent.fhyd7-1.fna.fbcdn.net/v/t39.30808-6/789301249_122130836990365627_2241280489982577902_n.jpg?stp=dst-jpg_tt6&cstp=mx1536x1024&ctp=s1536x1024&_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=9i0dApIdFM8Q7kNvwFf0XZN&_nc_oc=Adrk_InvfzrE_NdzawI9ZLQhME8VzSiTRIlQzpPn5iTufz6oUUuhoFtDmL7DbltSDNc&_nc_zt=23&_nc_ht=scontent.fhyd7-1.fna&_nc_gid=pTiOFtNh4lMYjBSjqIO-RQ&_nc_ss=7b2a8&oh=00_AQK1REII-6-9rox-G5B4AuV4SCGLG3WYx8cHaG0QlEAAXQ&oe=6A9A34B6',
+        src: 'https://scontent.fhyd7-1.fna.fbcdn.net/v/t39.30808-6/779957641_122130962228365627_2655053558510186934_n.jpg?stp=dst-jpg_tt6&cstp=mx1024x1536&ctp=s1024x1536&_nc_cat=105&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=127cfc&_nc_ohc=AFtbgNQvpr4Q7kNvwH8XwRt&_nc_oc=AdqoaIsbNzt-59WQeCzM9ef_sPt_bKiuQPb_k5ZE_HS6DZ-OxS04MBtTzp6ZW7e5pxI&_nc_zt=23&_nc_ht=scontent.fhyd7-1.fna&_nc_gid=HFZ9dj47glhXiOZJV7r62w&_nc_ss=7b2a8&oh=00_AQIIAfmNrTo0Evya9JB7eAilulTPwlwThoeBkC3-DvOsaA&oe=6AA309AE',
         alt: 'Festival Lights',
       },
       {
         src: 'https://tse1.mm.bing.net/th/id/OIP.ObiViaunjoNYKSCf9zJXDgHaGZ?r=0&rs=1&pid=ImgDetMain&o=7&rm=3',
         alt: 'Festival Decoration',
       },
-        {
-        src: 'https://lh3.googleusercontent.com/gps-cs-s/AHRPTWkImTjsF1AFT15fU6Nzum6Gn12-jalgnE3eWRyiDkKk5bsur8d5uDl2VG_mFZd3627ciVRYKYbb_nszpwugXUGCcJdI5bmHRjVZAYKHtNGnIIw4uFg8qqUH-zjrMt6kdVQ6Ygsv2L4mbgH6=s527-k-no',
+      {
+        src: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTaBwdjHUsEEXx1n20QBiaYaMDBLC04902-ioT9gHe13CXTINVSbMk-FEOdz-jHPOUcHPPN2_kc5Xr6uu3wZFgUppEHr8MjE4kRc6b2aA3m',
         alt: 'Shivyoga Shop Products',
       },
-
-      
     ],
-
   },
 
   // ---------------------------------------------------
@@ -137,11 +126,10 @@ const galleryGroups = [
         src: 'https://cdn.corenexis.com/f/zuH2RAgmQdf.png',
         alt: 'Electrical Work',
       },
-       {
+      {
         src: 'https://cdn.corenexis.com/f/G3RnockAi9F.png',
         alt: 'Electrical Work',
       },
-   
     ],
   },
 ]
@@ -159,28 +147,27 @@ export default function Gallery() {
   // त्या group मधली कोणती image open आहे?
   const [activeImageIndex, setActiveImageIndex] = useState(0)
 
-  // ---------------------------------------------------
+  // =====================================================
   // OPEN GROUP
-  // ---------------------------------------------------
+  // =====================================================
 
   const openGroup = (group) => {
     setActiveGroup(group)
     setActiveImageIndex(0)
   }
 
-  // ---------------------------------------------------
+  // =====================================================
   // CLOSE
-  // ---------------------------------------------------
+  // =====================================================
 
   const closeGallery = () => {
     setActiveGroup(null)
     setActiveImageIndex(0)
   }
 
-  // ---------------------------------------------------
+  // =====================================================
   // NEXT
-  // फक्त activeGroup मधल्या images वर काम करेल
-  // ---------------------------------------------------
+  // =====================================================
 
   const nextImage = () => {
     if (!activeGroup) return
@@ -194,10 +181,9 @@ export default function Gallery() {
     })
   }
 
-  // ---------------------------------------------------
+  // =====================================================
   // PREVIOUS
-  // फक्त activeGroup मधल्या images वर काम करेल
-  // ---------------------------------------------------
+  // =====================================================
 
   const previousImage = () => {
     if (!activeGroup) return
@@ -211,9 +197,9 @@ export default function Gallery() {
     })
   }
 
-  // ---------------------------------------------------
+  // =====================================================
   // KEYBOARD
-  // ---------------------------------------------------
+  // =====================================================
 
   useEffect(() => {
     if (!activeGroup) return
@@ -244,9 +230,35 @@ export default function Gallery() {
     }
   }, [activeGroup])
 
-  // ---------------------------------------------------
+  // =====================================================
+  // AUTO IMAGE SCROLL
+  // प्रत्येक 3 सेकंदांनी next image
+  // =====================================================
+
+  useEffect(() => {
+    if (!activeGroup) return
+
+    // एकच image असेल तर auto-scroll नको
+    if (activeGroup.images.length <= 1) return
+
+    const interval = setInterval(() => {
+      setActiveImageIndex((current) => {
+        if (current === activeGroup.images.length - 1) {
+          return 0
+        }
+
+        return current + 1
+      })
+    }, 3000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [activeGroup])
+
+  // =====================================================
   // ACTIVE IMAGE
-  // ---------------------------------------------------
+  // =====================================================
 
   const activeImage = activeGroup
     ? activeGroup.images[activeImageIndex]
@@ -259,13 +271,11 @@ export default function Gallery() {
       ================================================= */}
 
       <section id="gallery" className="bg-white">
-
         <div className="max-w-7xl mx-auto px-4 md:px-6 py-16">
 
           {/* HEADER */}
 
           <div className="text-center max-w-2xl mx-auto mb-10">
-
             <span className="eyebrow justify-center">
               {t('gallery_eyebrow')}
             </span>
@@ -277,7 +287,6 @@ export default function Gallery() {
             <p className="mt-2 text-navy-400/80 font-marathi text-sm">
               {t('gallery_subtitle')}
             </p>
-
           </div>
 
           {/* =================================================
@@ -285,9 +294,7 @@ export default function Gallery() {
           ================================================= */}
 
           <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4">
-
             {galleryGroups.map((group) => {
-
               const label = pick(
                 group.labelMr,
                 group.labelEn
@@ -301,7 +308,6 @@ export default function Gallery() {
                   className="group relative aspect-square rounded-xl overflow-hidden bg-navy-50 focus-visible:ring-2 focus-visible:ring-gold-500"
                   aria-label={label}
                 >
-
                   {/* COVER IMAGE */}
 
                   <img
@@ -328,11 +334,9 @@ export default function Gallery() {
                   <span className="absolute bottom-0 inset-x-0 bg-navy-700/80 text-white text-[11px] font-marathi py-1.5 text-center">
                     {label}
                   </span>
-
                 </button>
               )
             })}
-
           </div>
         </div>
       </section>
@@ -349,12 +353,14 @@ export default function Gallery() {
           onClick={closeGallery}
         >
 
-          {/* CLOSE */}
+          {/* =================================================
+              CLOSE
+          ================================================= */}
 
           <button
             type="button"
             onClick={closeGallery}
-            className="absolute top-4 right-4 z-[120] h-11 w-11 rounded-full bg-white/15 hover:bg-white/30 text-white text-3xl flex items-center justify-center"
+            className="absolute top-4 right-4 z-[120] h-11 w-11 rounded-full bg-white/15 hover:bg-white/30 text-white text-3xl flex items-center justify-center transition-all duration-300 hover:rotate-90 hover:scale-110 active:scale-95"
             aria-label={t('gallery_close')}
           >
             ×
@@ -372,7 +378,6 @@ export default function Gallery() {
 
           {/* =================================================
               PREVIOUS
-              फक्त group मध्ये 2+ images असतील तेव्हाच
           ================================================= */}
 
           {activeGroup.images.length > 1 && (
@@ -382,7 +387,7 @@ export default function Gallery() {
                 e.stopPropagation()
                 previousImage()
               }}
-              className="absolute left-3 md:left-8 z-[120] h-12 w-12 md:h-14 md:w-14 rounded-full bg-white/15 hover:bg-white/30 text-white text-4xl flex items-center justify-center transition"
+              className="absolute left-3 md:left-8 z-[120] h-12 w-12 md:h-14 md:w-14 rounded-full bg-white/15 hover:bg-white/30 text-white text-4xl flex items-center justify-center transition-all duration-300 hover:scale-110 active:scale-90"
               aria-label="Previous image"
             >
               ‹
@@ -399,9 +404,10 @@ export default function Gallery() {
           >
 
             <img
+              key={`${activeGroup.id}-${activeImageIndex}`}
               src={activeImage.src}
               alt={activeImage.alt}
-              className="max-h-[75vh] max-w-full object-contain rounded-xl shadow-2xl"
+              className="max-h-[75vh] max-w-full object-contain rounded-xl shadow-2xl animate-gallery-image"
             />
 
             {/* GROUP NAME */}
@@ -413,11 +419,32 @@ export default function Gallery() {
               )}
             </p>
 
+            {/* =================================================
+                DOT INDICATORS
+            ================================================= */}
+
+            {activeGroup.images.length > 1 && (
+              <div className="flex items-center justify-center gap-1.5 mt-4">
+                {activeGroup.images.map((_, index) => (
+                  <button
+                    key={index}
+                    type="button"
+                    onClick={() => setActiveImageIndex(index)}
+                    className={`rounded-full transition-all duration-300 ${
+                      index === activeImageIndex
+                        ? 'w-6 h-2 bg-white'
+                        : 'w-2 h-2 bg-white/40 hover:bg-white/70'
+                    }`}
+                    aria-label={`Go to image ${index + 1}`}
+                  />
+                ))}
+              </div>
+            )}
+
           </div>
 
           {/* =================================================
               NEXT
-              फक्त group मध्ये 2+ images असतील तेव्हाच
           ================================================= */}
 
           {activeGroup.images.length > 1 && (
@@ -427,15 +454,71 @@ export default function Gallery() {
                 e.stopPropagation()
                 nextImage()
               }}
-              className="absolute right-3 md:right-8 z-[120] h-12 w-12 md:h-14 md:w-14 rounded-full bg-white/15 hover:bg-white/30 text-white text-4xl flex items-center justify-center transition"
+              className="absolute right-3 md:right-8 z-[120] h-12 w-12 md:h-14 md:w-14 rounded-full bg-white/15 hover:bg-white/30 text-white text-4xl flex items-center justify-center transition-all duration-300 hover:scale-110 active:scale-90"
               aria-label="Next image"
             >
               ›
             </button>
           )}
 
+          {/* =================================================
+              AUTO-SCROLL PROGRESS
+          ================================================= */}
+
+          {activeGroup.images.length > 1 && (
+            <div className="absolute bottom-4 left-1/2 -translate-x-1/2 w-32 md:w-48 h-1 bg-white/20 rounded-full overflow-hidden">
+              <div
+                key={activeImageIndex}
+                className="h-full bg-white rounded-full animate-gallery-progress"
+              />
+            </div>
+          )}
+
         </div>
       )}
+
+      {/* =================================================
+          ANIMATIONS
+      ================================================= */}
+
+      <style>{`
+        @keyframes galleryImageIn {
+          from {
+            opacity: 0;
+            transform: scale(0.96);
+          }
+
+          to {
+            opacity: 1;
+            transform: scale(1);
+          }
+        }
+
+        @keyframes galleryProgress {
+          from {
+            width: 0%;
+          }
+
+          to {
+            width: 100%;
+          }
+        }
+
+        .animate-gallery-image {
+          animation: galleryImageIn 0.45s ease-out;
+        }
+
+        .animate-gallery-progress {
+          animation: galleryProgress 3s linear;
+        }
+
+        @media (prefers-reduced-motion: reduce) {
+          .animate-gallery-image,
+          .animate-gallery-progress {
+            animation: none;
+          }
+        }
+      `}</style>
     </>
   )
 }
