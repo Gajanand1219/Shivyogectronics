94507b76346338d2c0d1617d1c757d85727473c7
Gajanan Deshmukh <159412495+Gajanand1219@users.noreply.github.com>
2026-09-08 15:42:02 +0530

    Remove server proxy configuration
    Removed proxy configuration for the server.

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
   server: {
     host: '0.0.0.0',
-
-    proxy: {
-      '/api': {
-        target: 'https://shivyogbackend-rizm.onrender.com/',
-        changeOrigin: true,
-        secure: false,
-
-        rewrite: (path) =>
-          path.replace(/^\/api/, ''),
-      },
-    },
   },
 })
